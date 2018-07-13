1e6a8a63681a701e4a3684a94bc59a59719851b2
Isaac <ihookom@callibrity.com>
2018-07-12 21:45:50 -0400

    datastore client started

--- src/main/kotlin/com/undeaddarts/Controller.kt
+++ src/main/kotlin/com/undeaddarts/Controller.kt
@@ -1,13 +1,32 @@
 package com.undeaddarts
 
+import com.google.cloud.datastore.DatastoreOptions
+import com.google.cloud.datastore.Query
 import org.springframework.web.bind.annotation.*
 
 data class Message(val text: String, val priority: String)
 
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
 class MessageController {
-    @RequestMapping("/message")
+    @RequestMapping("/stats")
     fun message(): Message {
+        val datastore = DatastoreOptions
+                .newBuilder()
+                .setProjectId("undead-darts-1")
+                .build()
+                .service
+
+        val query = Query
+                .newEntityQueryBuilder()
+                .setKind("PlayerStatTest")
+                .build()
+
+        val results = datastore.run(query)
+
+        results.forEach {
+            println(it)
+        }
         return Message("Hello from Google Cloud", "Low")
     }
 }
